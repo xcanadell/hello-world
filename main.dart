657a9eb5e311fbd1979f79677cb7b87a423c3f9f
xcanadell <xcanadell@gmail.com>
2020-04-05 16:13:15 +0200

    tesyt

--- main.dart
+++ main.dart
@@ -3,6 +3,6 @@ import 'dart:html';
 void main() {
   var header = querySelector('#header');
   header.text = "Hello, World!";
-  
-  print(“Metodo principal”);
+  var header2 = querySelector('#header2');
+  header2.text = "H2 dinamico";
 }
